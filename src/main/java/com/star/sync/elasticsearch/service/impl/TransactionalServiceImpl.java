ae2072dce70e0aced59573ba6111a2890061559e
luxingxiao <luxingxiao@new4g.cn>
2018-10-30 19:47:02 +0800

    将下划线转为驼峰样式

--- src/main/java/com/star/sync/elasticsearch/service/impl/TransactionalServiceImpl.java
+++ src/main/java/com/star/sync/elasticsearch/service/impl/TransactionalServiceImpl.java
@@ -5,12 +5,15 @@ import com.star.sync.elasticsearch.model.IndexTypeModel;
 import com.star.sync.elasticsearch.model.request.SyncByTableRequest;
 import com.star.sync.elasticsearch.service.ElasticsearchService;
 import com.star.sync.elasticsearch.service.TransactionalService;
+
+import org.apache.commons.lang.text.StrBuilder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.Resource;
 
 import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 import java.time.ZoneId;
@@ -26,32 +29,50 @@ import java.util.stream.Collectors;
 @Service
 public class TransactionalServiceImpl implements TransactionalService {
 
-    @Resource
-    private BaseDao baseDao;
+	@Resource
+	private BaseDao baseDao;
 
-    @Resource
-    private ElasticsearchService elasticsearchService;
+	@Resource
+	private ElasticsearchService elasticsearchService;
 
-    @Transactional(readOnly = true, rollbackFor = Exception.class)
-    @Override
-    public void batchInsertElasticsearch(SyncByTableRequest request, String primaryKey, long from, long to, IndexTypeModel indexTypeModel) {
-        List<Map<String, Object>> dataList = baseDao.selectByPKIntervalLockInShareMode(primaryKey, from, to, request.getDatabase(), request.getTable());
-        if (dataList == null || dataList.isEmpty()) {
-            return;
-        }
-        dataList = convertDateType(dataList);
-        Map<String, Map<String, Object>> dataMap = dataList.parallelStream().collect(Collectors.toMap(strObjMap -> String.valueOf(strObjMap.get(primaryKey)), map -> map));
-        elasticsearchService.batchInsertById(indexTypeModel.getIndex(), indexTypeModel.getType(), dataMap);
-    }
+	@Transactional(readOnly = true, rollbackFor = Exception.class)
+	@Override
+	public void batchInsertElasticsearch(SyncByTableRequest request, String primaryKey, long from, long to,
+			IndexTypeModel indexTypeModel) {
+		List<Map<String, Object>> dataList = baseDao.selectByPKIntervalLockInShareMode(primaryKey, from, to,
+				request.getDatabase(), request.getTable());
+		if (dataList == null || dataList.isEmpty()) {
+			return;
+		}
+		dataList = convertDateType(dataList);
+		Map<String, Map<String, Object>> dataMap = dataList.parallelStream()
+				.collect(Collectors.toMap(strObjMap -> String.valueOf(strObjMap.get(primaryKey)), map -> map));
+		elasticsearchService.batchInsertById(indexTypeModel.getIndex(), indexTypeModel.getType(), dataMap);
+	}
 
-    private List<Map<String, Object>> convertDateType(List<Map<String, Object>> source) {
-        source.parallelStream().forEach(map -> map.forEach((key, value) -> {
-            if (value instanceof Timestamp) {
-                map.put(key, LocalDateTime.ofInstant(((Timestamp) value).toInstant(), ZoneId.systemDefault()));
-            }else if(value instanceof BigDecimal) {
-            	map.put(key, ((BigDecimal)value).toPlainString());
-            }
-        }));
-        return source;
-    }
+	private List<Map<String, Object>> convertDateType(List<Map<String, Object>> source) {
+		source.parallelStream().forEach(map -> map.forEach((key, value) -> {
+			if (value instanceof Timestamp) {
+				map.put(key, LocalDateTime.ofInstant(((Timestamp) value).toInstant(), ZoneId.systemDefault()));
+			} else if (value instanceof BigDecimal) {
+				map.put(key, ((BigDecimal) value).toPlainString());
+			} else if (value instanceof java.math.BigInteger) {
+				map.put(key, ((BigInteger) value).longValue());
+			}
+
+			if (key.contains("_")) {
+				StringBuffer stringBuffer = new StringBuffer();
+				String[] fragments = key.split("_");
+				stringBuffer.append(fragments[0]);
+				for (int i = 1; i < fragments.length; i++) {
+					Character.toUpperCase(fragments[i].indexOf(0));
+					stringBuffer.append(fragments[i]);
+				}
+				String newKey = stringBuffer.toString();
+				map.put(newKey, value);
+				map.remove(key);
+			}
+		}));
+		return source;
+	}
 }
